a0dc001b8cb6e5d6dde36e8df815107045c59023
vmazurova <vikimazurova@gmail.com>
2024-10-10 18:47:54 +0200

    start of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+// import {BrowserRouter as Router, Route}
 
+
+import Homepage from './pages/HomePage'
+import Bookingpage from './pages/BookingPage'
+import Coursepage from './pages/CoursePage'
+import Mealplanpage from './pages/MealPlanPage'
+import Memberdetailpage from './pages/MemberDetailPage'
+import Paymentpage from './pages/PaymentPage'
+import Bookingpage from './pages/BookingPage'
+import Navbar from './components/Navbars/AdminNavbar'
 
 function App() {
   return (
     <div className="App">
-     
+     <Switch>
+        {/* <Route path={`/auth`} component={AuthLayout} />
+      <Route path={`/admin`} component={AdminLayout} />
+      <Redirect from={`/`} to='/admin/dashboard' />*/}
+        <Route path= '/'><Homepage/></Route>
+    </Switch>
     </div>
   );
 }
